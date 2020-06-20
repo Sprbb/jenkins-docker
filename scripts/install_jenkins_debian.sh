b6daf4ee8254a9e30f0cd243c9964cdc46ed1c59
Bas Neve <bastiaanneve@gmail.com>
2020-06-20 18:19:47 +0200

    Added install_jenkins_debian.sh

--- scripts/install_jenkins_debian.sh
+++ scripts/install_jenkins_debian.sh
@@ -8,7 +8,7 @@ apt -y install software-properties-common dirmngr apt-transport-https lsb-releas
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
-apt-add-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-add-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
 systemctl enable docker
